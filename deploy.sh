f60810ba70bb365a967e7fc798adfbc4293d2314
Hutao <327218150@qq.com>
2023-10-23 17:17:01 +0800

    Update Files

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,7 @@
 set -e
 
 # 构建
-<<<<<<< HEAD
 # npm run build
-=======
-npm run build
->>>>>>> origin/master
 
 # cd 到构建输出的目录下
 cd dist
@@ -21,17 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-<<<<<<< HEAD
 git push -f git@github.com:miuimod/miuimod.github.io.git master:pages
-=======
-git push -f git@github.com:miuimod/miuimod.github.io.git master:gh-pages
->>>>>>> origin/master
-
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-<<<<<<< HEAD
 cd -
-=======
-cd -
->>>>>>> origin/master
+
